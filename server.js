8a73d616a2899c8b478e01bdfcb3b905a34186e3
Chris Williams <chris@iterativedesigns.com>
2010-09-20 23:34:07 -0400

    Initial code release.

---
+++ server.js
@@ -0,0 +1,65 @@
+var connect = require("connect");
+var express = require("express");
+
+var global_objects = {
+    "Array": ["isArray", "constructor", "index", "input", "length", "pop", "push", "reverse", "shift", "sort", "splice", "unshift", "concat", "join", "slice", "toString", "indexOf", "lastIndexOf", "forEach", "map", "some", "every", "filter", "Creating an Array", "Example: Creating a Two-dimensional Array"],
+    "String": ["prototype", "fromCharCode", "constructor", "length", "charAt", "concat", "indexOf", "lastIndexOf", "localeCompare", "match", "replace", "search", "slice", "split", "substr", "substring", "toLocaleLowerCase", "toLocaleUpperCase","toLowerCase", "toString", "toUpperCase", "valueOf"],
+    "Number": ["toExponential", "toFixed", "toLocaleString", "toPrecision", "toString", "valueOf", "Example: Using the Number object to assign values to numeric variables", "Example: Using Number to convert a Date object"],
+    "RegExp": ["constructor", "global", "ignoreCase", "lastIndex", "multiline", "source", "exec", "test", "toString", "Example: Using a regular expression to change data format", "Example: Using a regular expression with the sticky flag"],
+    "Function": ["prototype", "arguments", "arity", "constructor", "length", "apply", "call", "toString", "Example: Specifying arguments with the Function constructor"]
+};
+
+
+
+var combinations = [];
+
+for (var i in global_objects) { 
+    var attrs = global_objects[i];
+    for (var idx in attrs) {
+        var val = attrs[idx];
+        var seo_string = ["JavaScript JS Documentation: JS ",i," ",val,", JavaScript ", i, " ", val];
+        if (val.indexOf(" ") < 0) {
+            seo_string = seo_string.concat([", JS ",i, " .",val,", JavaScript ", i, " .", val]);
+        }
+        combinations.push([seo_string.join(""), "https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/"+i]);
+    }
+}
+
+
+// can add any other SEO string -> URL combinations here.
+
+
+var pub = __dirname + '/public';
+var app = express.createServer(
+    express.staticProvider(pub)
+);
+
+
+app.set('views', __dirname + '/views');
+
+var counter = 0;
+
+app.get('/', function(req, res){
+    counter += 1;
+        
+    var alt_string = "JS Tutorial, JavaScript Tutorial, JavaScript Guide, Learn JavaScript JS, How To Learn JS, Learning JavaScript";
+    var href_string = "https://developer.mozilla.org/en/JavaScript/Guide";
+    if (counter % 10 == 0) {
+        alt_string = "JavaScript Reference, JavaScript Guide, JavaScript API, JS API, JS Guide, JS Reference, Learn JS, JS Documentation";
+        href_string = "https://developer.mozilla.org/en/JavaScript";
+    } else if (counter % 5 != 0) {
+        var combo = combinations[Math.floor(Math.random()*combinations.length)];
+        alt_string = combo[0];
+        href_string = combo[1];
+    }
+
+    res.render('index.ejs', {
+        locals: {
+            alt: alt_string,
+            href: href_string
+        }
+    });
+});
+
+app.listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
